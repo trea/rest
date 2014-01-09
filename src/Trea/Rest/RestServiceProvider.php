09c2eb22cd9946444ad644262f42fa8bfcbab100
Trea Hauet <trea@treahauet.com>
2014-01-09 03:36:29 -0700

    PSR-2, Added Laravel Paginator and Eloquent Collection support

--- src/Trea/Rest/RestServiceProvider.php
+++ src/Trea/Rest/RestServiceProvider.php
@@ -2,39 +2,35 @@
 
 use Illuminate\Support\ServiceProvider;
 
-class RestServiceProvider extends ServiceProvider {
+class RestServiceProvider extends ServiceProvider
+{
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->app['rest'] = function() {
-			return new Rest();
-		};
-	}
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app['rest'] = function () {
+            return new Rest();
+        };
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
-
-<<<<<<< HEAD
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
 }
-=======
-}
->>>>>>> master
